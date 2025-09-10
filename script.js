d44464e5010ca88300a50d244c9b18daeefe9862
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2025-09-10 16:24:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
 function firstWord(s) {
   // your code here
-	let res= s.split(" ");
-	return res[0];
+	let s= s.trim();
+
+	if(s.length === 0)   return " ";
+	
+	let idx= s.indexOf(" ");
+
+	if(idx== -1)   return s;
+	
+	return s.substr(0,idx);
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
-
-
-
-
-
