6a298f2d31e61604260820afc3c4e2ffbf044871
Sayali Patil <137858267+sayalii17@users.noreply.github.com>
2025-09-10 16:14:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 function firstWord(s) {
   // your code here
+	let res= s.split(" ");
+	return res[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
+
+
+
+
